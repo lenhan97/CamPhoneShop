692056d4a5e20e7defe0f26b7af7e0d1b4d76d20
Lãm Hàn <lamhan1997@gmail.com>
2018-12-14 01:28:04 +0700

    Project

---
+++ ThuongMaiDienTuAPI/Dtos/PhanLoaiSPDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ThuongMaiDienTuAPI.Dtos
+{
+    public class PhanLoaiSPDto
+    {
+        public int SoLuong { get; set; }
+        public int? GiaKM { get; set; }
+        public int GiaBan { get; set; }
+        public int GiaGoc { get; set; }
+        public string Mau { get; set; }
+        public int IdKhuyenMai { get; set; }
+    }
+}
---
+++ ThuongMaiDienTuAPI/Entities/DanhMucSP.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ThuongMaiDienTuAPI.Entities
+{
+    public class DanhMucSP
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string Ten { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string TenKhac { get; set; }
+        public bool TinhTrang { get; set; }
+        [MaxLength(256)]
+        public string Hinh { get; set; }
+    }
+}
